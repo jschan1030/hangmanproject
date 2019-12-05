ef554c4fc03730d05bfd7ad301d1c18d8023d74b
jschan1030 <jschan1030@email.arizona.edu>
2019-12-05 13:03:31 -0700

    controller structure change 1

--- controller.php
+++ controller.php
@@ -1,5 +1,19 @@
 <?php
+
 include 'databaseadaptor.php';
+//create new database object
 $DBA = new DatabaseAdaptor();
 
+$state = $_GET ['var'];
+$username = $_GET ['username'];
+$password = $_GET ['password'];
+//Accounts
+function checkCredentials(){
+    //will return the 
+}
+//Log In
+
+//Register
+
+//Game
 ?>
