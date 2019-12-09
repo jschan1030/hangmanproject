5f51be3884b07b6b9ffb30351cc81fc331f2d23f
jschan1030 <jschan1030@email.arizona.edu>
2019-12-08 22:10:51 -0700

    controller log in

--- controller.php
+++ controller.php
@@ -1,71 +1,20 @@
 <?php
-
 include 'databaseadaptor.php';
-//create new database object
 $DBA = new DatabaseAdaptor();
-
-$state = $_POST ['var'];
-$username = $_POST ['username'];
-$password = $_POST ['password'];
-$user_id = $_POST ['id'];
-$email = $_POST ['email'];
-//Accounts
-    //logging in
-function checkCredentials($username, $password){
-    //will return true or false if the given credentials are in the accounts table in hangman database
-    $matches = $DBA->getCredentials($username, $password);
-    if (empty($matches)) {
-        return false;
-    }
-    else {
-        return true;
+$username = $_POST['username'];
+$password = $_POST['password'];
+if (! empty($_POST)) {
+    if (isset($username) && isset($password)) {
+        $user = $DBA->getCredentials($username, $password);
+        if (password_verify($_POST['password'], $user->password)) {
+            $_SESSION['user_id'] = $user->ID;
+        }
+        else{
+            echo("Error user/pass does not exist");
+        }
     }
 }
-
-//Log In
-function logIn($username, $password) {
-    if (checkCredentials == true) {
-        echo 'golden';
-    }
-    else {
-        echo 'incorrect credentials';
-    }
+else {
+    echo("Error user/pass does not exist");
 }
-//Register
-    //new user registration
-function newUser($username, $password, $email) {
-    //adds a new user to the accounts table in hangman database
-    $table = "accounts";
-    $category = "username";
-    $matches = $DBA->checkUniqueness($table, $category, $username);
-    if (empty($matches)) {
-        $DBA->newCredentials($username, $password, $email);
-        echo "new user information added successfuly";
-    }
-    else {
-        echo "new user information NOT added successfully";
-    }
-}
-
-function changeInfo() {
-
-}
-
-//Game
-
-function addtoWords_Phrases($difficulty, $entry, $type,$hint_entry, $id) {
-    $table = "words_phrases";
-    $category = "entry";
-    $matches = $DBA->checkUniqueness($table, $category, $entry);
-    if (empty($matches)) {
-        $DBA->addWordPhrase($difficulty, $type, $entry, $hint_entry, $id); 
-        echo "new word/phrase accepted";
-    }
-    else {
-        echo "new word/phrase NOT accepted";
-    }
-    $DBA->addWordPhrase();//FINISH THIS
-}
-
-
 ?>
