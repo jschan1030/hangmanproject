f331f16af3bb904f2ec74a3539e0c8205ef77694
jschan1030 <jschan1030@email.arizona.edu>
2019-12-09 20:36:30 -0700

    more gamehome changes

--- html_php/gamehome.php
+++ html_php/gamehome.php
@@ -1,4 +1,9 @@
 <?php
     include('header.php');
+    // In this file, the game can be displayed here, with different links to dynamically change the site as well as the
+    // game board. This will be a complicated file with lots of php scripts inside html.
 ?>
-<div>
+
+<?php
+    include('footer.php');
+?>
